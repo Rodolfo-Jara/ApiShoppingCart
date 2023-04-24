01a14cffced2f25ef576b5f0b19af0f1f6c37dec
Rodolfo Jara Lopez <rjaralopez19@gmail.com>
2023-04-24 15:33:29 -0500

    segundo commit

---
+++ src/main/java/pe/cibertec/ecommerce/ApiShoppingCart/entity/Customer.java
@@ -0,0 +1,15 @@
+
+package pe.cibertec.ecommerce.ApiShoppingCart.entity;
+
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+@Data
+@Embeddable
+public class Customer {
+    private Long customerId;
+    private String name;
+    private String email;
+    private String phone;
+    
+}
